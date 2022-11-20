4d24a442a679d5fd743ddfb3337249f71c1b5e43
Mian Ahmad Basit <66495447+mianahmadbasit1@users.noreply.github.com>
2022-11-20 15:14:40 +0500

    nextjs 1st class

---
+++ pages/api/test.js
@@ -0,0 +1,7 @@
+// app.get("/api/test", (req, res) => {
+//   res.json({message:"Hi Ahmad"})
+// });
+
+export default(req,res) => {
+    res.json({message:"Welcome Mian Ahmad Basit its api get"})
+}
---
+++ pages/product.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+const Home = () => {
+  return (
+    <div>
+      Welcome to product page <br></br>
+      <Link href="/">
+        Go To Home
+      </Link>
+    </div>
+  );
+};
+
+export default Home;
